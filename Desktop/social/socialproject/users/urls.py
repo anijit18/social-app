96ac748d911e5928ee589f24fdfc7671556546bc
anijit18 <121957827+anijit18@users.noreply.github.com>
2024-08-27 18:58:54 +0530

    Delete Desktop directory

--- Desktop/social/socialproject/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_view
-
-urlpatterns = [
-    path('login/', views.user_login, name="login"),
-    path('logout/', views.logout_view, name='logout'),
-    path('', views.index, name="index"),
-]
